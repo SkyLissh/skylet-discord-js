2c1ed95b82d44449c70b1af3f3e784f4e67c2cc7
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2025-05-15 21:09:22 -0600

    🚀 Updated CD flow
    Updated the github workflow to use the cd file, also updated the eslint file

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier/flat";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es5,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-exports": "warn",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  }
+);
